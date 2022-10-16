02d0eaeed08e845c9089a7bb2b39357f478eaa5a
milliorn <scottmilliorn@gmail.com>
2022-10-16 13:29:53 -0700

    app.js

--- cloud-app/src/App.tsx
+++ cloud-app/src/App.tsx
@@ -1,9 +1,21 @@
-import './App.css';
+import About from "./components/About";
+import AllInOne from "./components/AllInOne";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Support from "./components/Support";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+      <AllInOne />
+      <Pricing />
+      <Footer />
+    </>
+  );
 }
