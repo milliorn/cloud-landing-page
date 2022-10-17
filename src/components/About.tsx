abf5d2e286dcca84b37fc00cd0e3ea653ce38428
milliorn <scottmilliorn@gmail.com>
2022-10-16 19:55:51 -0700

    About, mappedData

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,34 @@
 const About = () => {
+  const data = [
+    {
+      pText1: "100%",
+      pText2: "Completion",
+    },
+    {
+      pText1: "24/7",
+      pText2: "Delivery",
+    },
+    {
+      pText1: "100%",
+      pText2: "Transactions",
+    },
+  ];
+
+  const mappedData = data.map((e, i) => (
+    <div className="border py-8 rounded-xl shadow-xl" key={i}>
+      <p className="text-6xl font-bold text-indigo-600">{e.pText1}</p>
+      <p className="text-gray-400 mt-2">{e.pText2}</p>
+    </div>
+  ));
+
   return (
     <div id="about" className="w-full my-12 sm:my-32">
       <div className="max-w-[1240px] mx-auto">
         <div className="text-center">
-          <h2 className="sm:text-5xl text-2xl font-bold">
+          <h2 className="sm:text-5xl text-2xl font-bold mx-1">
             Trusted by developers across the world
           </h2>
-          <p className="sm:text-3xl py-6 text-gray-500">
+          <p className="sm:text-3xl py-6 text-gray-500 px-5 sm:px-4 md:px-3 lg:px-2 xl:px-1">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque
             perspiciatis qui, provident a hic nesciunt harum illum quae dolores.
             Inventore esse error ipsam illo repellat. Repellat libero
@@ -15,18 +37,7 @@ const About = () => {
         </div>
 
         <div className="grid md:grid-cols-3 gap-1 px-2 text-center">
-          <div className="border py-8 rounded-xl shadow-xl">
-            <p className="text-6xl font-bold text-indigo-600">100%</p>
-            <p className="text-gray-400 mt-2">Completion</p>
-          </div>
-          <div className="border py-8 rounded-xl shadow-xl">
-            <p className="text-6xl font-bold text-indigo-600">24/7</p>
-            <p className="text-gray-400 mt-2">Delivery</p>
-          </div>
-          <div className="border py-8 rounded-xl shadow-xl">
-            <p className="text-6xl font-bold text-indigo-600">100K</p>
-            <p className="text-gray-400 mt-2">Transactions</p>
-          </div>
+          {mappedData}
         </div>
       </div>
     </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
       id="home"
       className="w-full h-screen bg-zinc-200 flex flex-col justify-between"
     >
-      <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
+      <div className="grid md:grid-cols-2 max-w-[1280px] m-auto">
         <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
           <p className="text-2xl mt-2 sm:mt-0">
             Unique Sequencing & Production
@@ -23,7 +23,7 @@ const Hero = () => {
           <img className="w-full invisible sm:visible" src={bgImg} alt="/" />
         </div>
         <div
-          className="absolute flex flex-col sm:py-8 md:min-w-[760px] bottom-[5%]
+          className="absolute flex flex-col sm:py-8 md:min-w-[768px] bottom-[5%]
             mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200
             border border-slate-300 rounded-xl text-center shadow-xl -my-8 sm:my-0"
         >
