f59fd5453e36c0b24bdf0236a84bd5c35b6fb9d6
milliorn <scottmilliorn@gmail.com>
2022-11-01 01:53:11 -0700

    comments not found for functions and classes

--- src/components/FooterForm.tsx
+++ src/components/FooterForm.tsx
@@ -1,10 +1,12 @@
+/* form foound in footer to email */
 export function FooterForm(): JSX.Element {
   return (
     <form className="flex flex-col sm:flex-row">
       <input
         className="w-full p-2 mr-4 rounded-md mb-4"
         type="email"
-        placeholder="Enter email.." />
+        placeholder="Enter email.."
+      />
       <button type="button" className="p-2 mb-4 bg-indigo-700">
         Subscribe
       </button>
