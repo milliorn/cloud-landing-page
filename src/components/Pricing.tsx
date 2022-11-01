5d751c42ca703d40f2404f3acec427fcab3ebcaa
milliorn <scottmilliorn@gmail.com>
2022-11-01 02:29:47 -0700

    depth of nested jsx to be <= 2, but found 3

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,6 +1,7 @@
 import uuid from "react-uuid";
 import { CheckMark } from "./CheckMark";
 import { PricingCards } from "./PricingCards";
+import { PricingText } from "./PricingText";
 
 /* create lorem text */
 export function paraLorem({ text }: { text: string }): void {
@@ -27,20 +28,8 @@ export default function Pricing(): JSX.Element {
   return (
     <div id="pricing" className="w-full text-white my-24">
       <div className="w-full h-[800px] bg-slate-900 absolute mix-blend-overlay" />
-
       <div className="max-w-[1280px] mx-auto py-12">
-        <div className="text-center py-8 text-slate-300">
-          <h2 className="text-3xl uppercase">Pricing</h2>
-          <h3 className="text-2xl sm:text-5xl font-bold text-white py-8">
-            The right price for your research.
-          </h3>
-          <p className="sm:text-3xl">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque
-            eveniet provident ut. Possimus tempore quasi dolore maiores fuga
-            explicabo dolorum, facilis nisi doloribus? Maxime quis quia fugiat
-            libero sed quibusdam.
-          </p>
-        </div>
+        <PricingText />
         <PricingCards />
       </div>
     </div>
---
+++ src/components/PricingText.tsx
@@ -0,0 +1,19 @@
+/**
+ * container for all text in pricing element
+ */
+export function PricingText(): JSX.Element {
+  return (
+    <div className="text-center py-8 text-slate-300">
+      <h2 className="text-3xl uppercase">Pricing</h2>
+      <h3 className="text-2xl sm:text-5xl font-bold text-white py-8">
+        The right price for your research.
+      </h3>
+      <p className="sm:text-3xl">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque
+        eveniet provident ut. Possimus tempore quasi dolore maiores fuga
+        explicabo dolorum, facilis nisi doloribus? Maxime quis quia fugiat
+        libero sed quibusdam.
+      </p>
+    </div>
+  );
+}
