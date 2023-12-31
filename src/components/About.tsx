fc08a647c95eff77d3e7dbd25e1ad77a28a5c241
milliorn <scottmilliorn@gmail.com>
2023-12-31 14:58:58 -0800

    Update styling for About and Footer

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -22,7 +22,7 @@ function About(): JSX.Element {
   const mappedData = data.map((e) => (
     <div className="border py-8 rounded-xl shadow-xl" key={uuid()}>
       <p className="text-6xl font-bold text-indigo-600">{e.pText1}</p>
-      <p className="text-gray-400 mt-2">{e.pText2}</p>
+      <p className="text-gray-900 mt-2">{e.pText2}</p>
     </div>
   ));
 
--- src/components/FooterEmail.tsx
+++ src/components/FooterEmail.tsx
@@ -3,11 +3,11 @@ import { FooterForm } from "./FooterForm";
 /* container for email form */
 export function FooterEmail(): JSX.Element {
   return (
-    <div className="col-span-2 pt-8 md p-4:sm:pt-2">
-      <p className="font-bold uppercase text-center">
+    <div className="col-span-2 pt-8 md:pt-0 p-4">
+      <p className="font-bold uppercase text-center text-white">
         Subscribe to our newsletter
       </p>
-      <p className="py-4">
+      <p className="py-4 text-gray-200">
         The latest news, articles, and resources, sent to your inbox weekly.
       </p>
       <FooterForm />
