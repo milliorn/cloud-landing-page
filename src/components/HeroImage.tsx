77a01c54bf75b181b56fe32b3628c274df740e1c
milliorn <scottmilliorn@gmail.com>
2023-12-31 16:04:49 -0800

    Add image preload and update image sources

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -1,15 +1,11 @@
-import bgImg from "../assets/cyber-bg.webp";
-
 /* creates hero image */
 export function HeroImage(): JSX.Element {
   return (
     <div>
-      <img 
-        className="w-full invisible sm:visible" 
-        src={bgImg} 
-        alt="Descriptive alt text"
-        width={829}
-        height={600}
+      <img
+        className="w-full invisible sm:visible"
+        src="/cloud-landing-page/cyber-bg.webp"
+        alt="/"
       />
     </div>
   );
