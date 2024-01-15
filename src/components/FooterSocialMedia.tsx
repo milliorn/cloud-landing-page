4ee110a921387808f42439ebfd9fc9331b981123
milliorn <scottmilliorn@gmail.com>
2024-01-15 13:40:38 -0800

    Add cursor-pointer to social media buttons in FooterSocialMedia component

--- src/components/FooterSocialMedia.tsx
+++ src/components/FooterSocialMedia.tsx
@@ -9,7 +9,7 @@ import {
 /* create social media buttons in footer */
 export function FooterSocialMedia(): JSX.Element {
   return (
-    <div className="flex justify-between sm:w-[300px] pt-4 text-2xl">
+    <div className="flex justify-between sm:w-[300px] pt-4 text-2xl cursor-pointer">
       <FaFacebook />
       <FaInstagram />
       <FaTwitter />
