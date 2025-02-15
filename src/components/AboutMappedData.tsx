dd01c8155bebed7023d764534dd47110984706dc
milliorn <scottmilliorn@gmail.com>
2025-02-14 21:11:57 -0800

    fixed typed issue with jsx element

--- src/components/AboutMappedData.tsx
+++ src/components/AboutMappedData.tsx
@@ -1,9 +1,11 @@
+import { JSX } from "react";
+
 /* data populated in the about section */
 export function AboutMappedData({
   mappedData,
 }: {
-  mappedData[];
-}) {
+  mappedData: JSX.Element[];
+}): JSX.Element {
   return (
     <div className="grid md:grid-cols-3 gap-1 px-2 text-center">
       {mappedData}
