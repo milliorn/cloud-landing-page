19c931030c19e7f4139449b375ea73287a8d4886
milliorn <scottmilliorn@gmail.com>
2025-02-15 02:58:40 -0800

    lighthouse score

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -1,11 +1,12 @@
-/* creates hero image */
 export function HeroImage() {
   return (
     <div>
       <img
         className="w-full invisible sm:visible"
         src="/cyber-bg.webp"
-        alt="/"
+        alt="Cyber background"
+        width={829}
+        height={600}
       />
     </div>
   );
