3d5bd1e3402eb536cf07e0ff649c02b9583164c0
milliorn <scottmilliorn@gmail.com>
2025-03-14 23:44:34 -0700

    fix warnings in form

--- src/components/FooterForm.tsx
+++ src/components/FooterForm.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 
-/* form found in footer to email */
+/* Form found in footer to email */
 export function FooterForm() {
   return (
-    <form className="flex flex-col sm:flex-row">
+    <form
+      className="flex flex-col sm:flex-row"
+      action="/subscribe"
+      method="POST"
+      autoComplete="on"
+    >
       <input
+        id="email"
+        name="email"
         className="w-full p-2 mr-4 rounded-md mb-4 bg-white text-gray-900"
         type="email"
-        placeholder="Enter email.."
+        placeholder="Enter email..."
+        required
       />
-      <button type="button" className="p-2 mb-4 bg-indigo-700 text-white">
+      <button type="submit" className="p-2 mb-4 bg-indigo-700 text-white">
         Subscribe
       </button>
     </form>
