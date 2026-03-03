7764878275e7122e3dc632e1e0d1319b9c58138b
Scott Milliorn <scottmilliorn@gmail.com>
2026-03-02 23:30:16 -0800

    refactor: revert reversedCheckmarks

--- src/components/Pricing/PricingCardRight.tsx
+++ src/components/Pricing/PricingCardRight.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { mappeddataCheckmark } from ".";
 import { GetStarted } from "../GetStarted";
 
-const reversedCheckmarks = [...mappeddataCheckmark].reverse();
-
 /**
  * create pricing card on the right/bottom side (based on viewpoint)
  */
@@ -14,7 +12,7 @@ export function PricingCardRight() {
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste,
         adipisci? Sapiente.?
       </p>
-      {reversedCheckmarks}
+      {[...mappeddataCheckmark].reverse()}
       <GetStarted />
     </div>
   );
