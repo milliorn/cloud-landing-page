cb595e47acb8198524bfcf84a75ae089b634406a
Scott Milliorn <scottmilliorn@gmail.com>
2026-03-02 21:51:26 -0800

    Fix padding in footer component for consistent spacing

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -57,7 +57,7 @@ export default function Footer() {
   ));
 
   return (
-    <div className="w-full mt-24 bg-slate-900 py-y px-2">
+    <div className="w-full mt-24 bg-slate-900 py-8 px-2">
       <div className="max-w-[1280px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8">
         {mappedData}
         {FooterEmail()}
