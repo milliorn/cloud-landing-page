32fd2554e1b20110c8b3e5a8b844bc41128f286f
Scott Milliorn <scottmilliorn@gmail.com>
2026-03-02 23:08:37 -0800

    refactor: consolidate navbar links into a single array and remove redundant components

--- src/components/Navbar/NavbarLinks.tsx
+++ src/components/Navbar/NavbarLinks.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { NavbarLinksAbout } from "./NavbarLinksAbout";
-import { NavbarLinksHome } from "./NavbarLinksHome";
-import { NavbarLinksPlatforms } from "./NavbarLinksPlatforms";
-import { NavbarLinksPricing } from "./NavbarLinksPricing";
-import { NavbarLinksSupport } from "./NavbarLinksSupport";
+import { Link } from "react-scroll";
+
+export const navLinks = [
+  { to: "home", label: "Home", offset: 0 },
+  { to: "about", label: "About", offset: -200 },
+  { to: "support", label: "Support", offset: -50 },
+  { to: "platforms", label: "Platforms", offset: -100 },
+  { to: "pricing", label: "Pricing", offset: -50 },
+] as const;
 
 /**
  * create links found in leftside of navbar when expanded
@@ -15,21 +19,22 @@ export function NavbarLinks() {
         Lorem ipsum
       </h1>
       <ul className="hidden md:flex cursor-pointer">
-        <li>
-          <NavbarLinksHome />
-        </li>
-        <li>
-          <NavbarLinksAbout />
-        </li>
-        <li>
-          <NavbarLinksSupport />
-        </li>
-        <li>
-          <NavbarLinksPlatforms />
-        </li>
-        <li>
-          <NavbarLinksPricing />
-        </li>
+        {navLinks.map(({ to, label, offset }) => (
+          <li key={to}>
+            <Link
+              to={to}
+              smooth
+              offset={offset}
+              duration={500}
+              rel="noopener noreferrer"
+              role="link"
+              title={label}
+              aria-label={label}
+            >
+              {label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
